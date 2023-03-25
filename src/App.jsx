0c8c76ae8b8576238eb719963f1bcc14fb8da12c
YeduKrishnan A <kuttuzz17@gmail.com>
2023-03-25 11:08:19 +0530

    Display Five Question with Options

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,37 @@
 import { useState, useEffect } from 'react'
 import Menu from './Pages/Menu'
 import Game from './Pages/Game';
+import { nanoid } from 'nanoid'
 
 
 function App() {
-  const [apiCall, setApiCall] = useState('');
+  const [apiCall, setApiCall] = useState([]);
   const [isGameOn, SetIsGameOn] = useState(false);
-
-
-  function changeScreen() {
-    SetIsGameOn(prevIsGameOn => !prevIsGameOn)
-  }
-
-  function randomNumber() {
-    const rand = Math.floor(Math.random() * 5)
-    console.log(rand)
-    return rand;
-  }
 
   useEffect(() => {
     async function fetchAPI() {
       const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple");
       const data = await res.json();
-      // console.log(data.results)
-      setApiCall(data.results[randomNumber()])
-      // setApiCall(data.results)
+      // setApiCall(data.results[randomNumber()])
+      setApiCall(data.results)
     }
     fetchAPI()
   }, [])
 
-  // console.log(apiCall);
-  console.log(typeof ([apiCall]));
 
-  // const sike = apiCall.map(item => {
-  //   return (<Game
-  //     {...item}
-  //   // question={item.question}
-  //   />
-  //   )
-  // })
+  function changeScreen() {
+    SetIsGameOn(prevIsGameOn => !prevIsGameOn)
+  }
+  console.log(apiCall)
 
-  const sike =
-    <Game
-      {...apiCall}
-    // question={apiCall.question}
-    />
+  const sike = apiCall.map(item => {
+    return (
+      <Game
+        key={nanoid()}
+        {...item}
+      />
+    )
+  })
 
 
   return (
@@ -56,3 +43,11 @@ function App() {
 }
 
 export default App
+
+
+
+  // function randomNumber() {
+  //   const rand = Math.floor(Math.random() * 5)
+  //   console.log(rand)
+  //   return rand;
+  // }
