fe7cb7c22e72bc61a0f21f17f87ac71a6a4eff1f
YeduKrishnan A <kuttuzz17@gmail.com>
2023-03-26 14:37:11 +0530

    Only select one option for each question and apply dynamic style to it

--- src/App.jsx
+++ src/App.jsx
@@ -7,55 +7,72 @@ import { nanoid } from 'nanoid'
 function App() {
   const [apiCall, setApiCall] = useState([]);
   const [isGameOn, setIsGameOn] = useState(false);
-  const [holdOption, setHoldOption] = useState(true);
 
   useEffect(() => {
     async function fetchAPI() {
       const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple");
       const data = await res.json();
       // setApiCall(data.results[randomNumber()])
-      console.log('API CALLING')
+      // console.log(data.results)
       setApiCall(data.results)
+      setApiCall(prevApiCall => prevApiCall.map(item => {
+        return (
+          {
+            ...item,
+            isOnOne: false, isOnTwo: false, isOnThree: false, isOnFour: false,
+            idOne: nanoid(), idTwo: nanoid(), idThree: nanoid(), idFour: nanoid()
+          }
+        )
+      }))
     }
     fetchAPI()
   }, [])
 
-
-  function changeScreen() {
-    setIsGameOn(prevIsGameOn => !prevIsGameOn)
-  }
-  console.log(holdOption)
-
-  function HandleHoldOption() {
-    setHoldOption(prevHoldOption => {
-      return (!prevHoldOption)
-    })
-  }
-
-  // function HandleHoldOption(id) {
-  //   console.log(id)
-  // }
-
-
   console.log(apiCall)
 
   const game = apiCall.map(item => {
+    // console.log(item.id)
     return (
       <Game
         key={nanoid()}
-        holdOption={holdOption}
-        id={nanoid()}
-        handleHoldOption={() => HandleHoldOption()}
+        // id={item.id}
+        // on={true}
+        // toggle={() => toggleHandle(item.id)}
+        toggle={toggleHandle}
         {...item}
       />
     )
   })
 
 
+  function changeScreen() {
+    setIsGameOn(prevIsGameOn => !prevIsGameOn)
+    console.clear()
+
+  }
+
+
+  function toggleHandle(id) {
+    console.clear()
+    // console.log(id)
+    setApiCall(prevApiCall => prevApiCall.map(item => {
+      // console.log(item.idOne)
+      // For each option we assign a id and check if that item id [item.id] matches the click element id [id]
+      // if so return every other value in state as before but change the isON state for the item clicked 
+      // and return every other element isON as false [so only one element can be selected]
+      return (
+        item.idOne === id ? { ...item, isOnOne: !item.isOnOne, isOnTwo: false, isOnThree: false, isOnFour: false } : item &&
+          item.idTwo === id ? { ...item, isOnOne: false, isOnTwo: !item.isOnTwo, isOnThree: false, isOnFour: false } : item &&
+            item.idThree === id ? { ...item, isOnOne: false, isOnTwo: false, isOnThree: !item.isOnThree, isOnFour: false } : item &&
+              item.idFour === id ? { ...item, isOnOne: false, isOnTwo: false, isOnThree: false, isOnFour: !item.isOnFour } : item
+      )
+    }))
+  }
+
+
   return (
     <div className="App">
       {isGameOn === false && <Menu onClick={() => changeScreen()} />}
-      {isGameOn === true && game}
       {isGameOn === true && game}
     </div>
   )
--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,37 +1,47 @@
 import { useEffect } from 'react';
 
-export { useEffect } from 'react'
-
 export default function Game(props) {
+    // console.log(props.on)
     const incorrect = props.incorrect_answers;
     const correct = props.correct_answer;
 
     let options = [...incorrect, correct];
-    let styles = { backgroundColor: props.holdOption ? 'green' : '' }
 
+    function styles(onValue) {
+        const style = { backgroundColor: onValue ? 'green' : '' }
+        return style
+    }
 
-    // function shuffleArray(array) {
+    // const stylesOne = {
+    //     backgroundColor: props.onOne ? 'green' : ''
+    // }
+    // const stylesTwo = {
+    //     backgroundColor: props.onTwo ? 'green' : ''
+    // }
+    // const stylesThree = {
+    //     backgroundColor: props.onThree ? 'green' : ''
+    // }
+    // const stylesFour = {
+    //     backgroundColor: props.onFour ? 'green' : ''
+    // }
+
     useEffect(() => {
         for (let i = options.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [options[i], options[j]] = [options[j], options[i]];
         }
     }, [])
-    // return array
-    // }
-    // shuffleArray(options);
-
 
     return (
         <div className="game">
-            <h2>Select Your Answers</h2>
+            {/* <h2>Select Your Answers</h2> */}
             <div className="gameQuestion">
                 <h3>{props.question}</h3>
                 <ul>
-                    <li style={styles} onClick={props.handleHoldOption} className="game-option">{options[0]}</li>
-                    <li className="game-option">{options[1]}</li>
-                    <li className="game-option">{options[2]}</li>
-                    <li className="game-option">{options[3]}</li>
+                    <li style={styles(props.isOnOne)} onClick={() => props.toggle(props.idOne)} className="game-option">{options[0]}</li>
+                    <li style={styles(props.isOnTwo)} onClick={() => props.toggle(props.idTwo)} className="game-option">{options[1]}</li>
+                    <li style={styles(props.isOnThree)} onClick={() => props.toggle(props.idThree)} className="game-option">{options[2]}</li>
+                    <li style={styles(props.isOnFour)} onClick={() => props.toggle(props.idFour)} className="game-option">{options[3]}</li>
                 </ul>
             </div>
         </div>
